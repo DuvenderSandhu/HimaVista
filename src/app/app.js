630d274c8921ccaa8156470e2e5221777ccaad25
D Sandhu <anusandhu666@gmail.com>
2024-05-26 23:11:26 +0530

    Initial Commit

---
+++ src/app/app.js
@@ -0,0 +1,8 @@
+import express from "express";
+import 'dotenv/config'
+import StaticRouter from "../../routes/StaticRouter.js";
+import LocalUserRouter from "../../routes/LocalUserRoute.js";
+const app = express()
+app.use('/api',StaticRouter)
+app.use('/api/localuser',LocalUserRouter)
+export default app 
